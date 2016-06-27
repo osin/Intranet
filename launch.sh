f9bb75517b052dae201eb1246a62119fff8cdd8d
Housseini Toumani <housseini.toumani@ysance.com>
2016-06-27 21:32:21 +0200

    retrait du mode detached

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 #You can launch this tools using osin/lamp Docker image
 #https://hub.docker.com/r/osin/lamp/
 
-docker run --rm --name intranet -v $(pwd)/web:/var/www/html -v $(pwd)/mysql:/var/lib/mysql -p 80:80 -p 3306:3306 osin/lamp
+docker run --name intranet -v $(pwd)/web:/var/www/html -v $(pwd)/mysql:/var/lib/mysql -p 80:80 -p 3306:3306 osin/lamp
